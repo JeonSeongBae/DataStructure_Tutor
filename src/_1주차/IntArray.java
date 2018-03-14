9d7a80759e61490f44e24d0bc41739a39ab2fa37
JeonSeongBae <tjdqo6042@naver.com>
2018-03-14 18:35:57 +0900

    _1 week array

---
+++ src/_1주차/IntArray.java
@@ -0,0 +1,48 @@
+package _1주차;
+
+import java.util.Random;
+
+public class IntArray {
+	int[] arr;
+	int index;
+
+	public IntArray(int size) {
+		this.arr = new int[size];
+		for (int i = 0; i < arr.length / 2; i++) {
+			Random random = new Random();
+			arr[i] = random.nextInt(10) + 1;
+		}
+		index = arr.length / 2;
+	}
+
+	public void add(int x) {
+		if (index > arr.length) {
+			resize();
+		}
+		arr[index++] = x;
+	}
+
+	private void resize() {
+		int[] temp = new int[arr.length];
+		System.arraycopy(arr, 0, temp, 0, arr.length);
+		arr = new int[arr.length * 2];
+		System.arraycopy(temp, 0, arr, 0, temp.length);
+	}
+
+	public void remove(int x) {
+		for (int i = 0; i < index; i++) {
+			if (arr[i] == x) {
+				System.arraycopy(arr, i + 1, arr, i, arr.length - i - 1);
+				index--;
+				break;
+			}
+		}
+	}
+
+	public void print() {
+		for (int i = 0; i < index; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
