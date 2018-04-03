a0a427a315d211d9e15bbdc24a87e6f42c2053fb
JeonSeongBae <tjdqo6042@naver.com>
2018-04-03 12:47:14 +0900

    _4_Linked List BigInt

--- src/_4주차/TestBigInt.java
+++ src/_4주차/TestBigInt.java
@@ -5,7 +5,11 @@ public class TestBigInt {
 	public static void main(String[] args) {
 		BigInt x = new BigInt(54321);
 		BigInt y = new BigInt(999);
-		System.out.println(x.plus(y));
+		BigInt z = new BigInt(9);
+		BigInt a = new BigInt(123);
+		System.out.println(x + " + " + y + " = " + (54321 + 999) + " ... " + x.plus(y));
+		System.out.println(x + " * " + z + " = " + 54321 * 9 + " ... " + x.multiplyone(z));
+		System.out.println(x + " * " + a + " = " + 54321 * 123 + " ... " + x.multiply(a));
+		System.out.println(y + " * " + a + " = " + 999 * 123 + " ... " + y.multiply(a));
 	}
-
 }
