c30dca44839dd50ec6b8f51154bafba8673076b7
JeonSeongBae <tjdqo6042@naver.com>
2018-04-16 20:53:54 +0900

    delete cached and add _5주차 homework

---
+++ src/_5주차_2/Node.java
@@ -0,0 +1,12 @@
+package _5주차_2;
+
+public class Node {
+    int value;
+    Node next;
+
+    Node(int value, Node next) {
+       this.value = value;
+       this.next = next;
+    }
+
+}
---
+++ src/_5주차_2/TestLinkedList.java
@@ -0,0 +1,23 @@
+package _5주차_2;
+
+public class TestLinkedList {
+
+	   public static void main(String[] args) {
+	      // TODO Auto-generated method stub
+	      LinkedList a = new LinkedList();
+	      
+	      for (int i = 1; i < 6; i++) {
+	         a.push(i);
+	      }
+	      for (int i = 1; i < 3; i++) {
+			System.out.println(a.pop());
+		}
+	      System.out.println(a);
+
+	      a.removesecond();
+	      System.out.println(a);
+	      
+	      a.reverse();
+	      System.out.println(a);
+	   }
+	}
