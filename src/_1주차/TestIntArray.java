323f9f517269a7f838694b9fad9651cb25a52d44
JeonSeongBae <tjdqo6042@naver.com>
2018-04-16 20:54:58 +0900

    all files update

---
+++ src/_1주차/TestIntArray.java
@@ -0,0 +1,13 @@
+package _1주차;
+
+public class TestIntArray {
+
+	public static void main(String[] args) {
+		IntArray intarray = new IntArray(10);
+		intarray.add(1);
+		intarray.add(2);
+		intarray.remove(2);
+		intarray.print();
+	}
+
+}
