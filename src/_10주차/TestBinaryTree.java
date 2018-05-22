f216dc13cbce1c67a1d99b5e4214fe1aa710722f
JeonSeongBae <tjdqo6042@naver.com>
2018-05-22 22:06:55 +0900

    TestCode_add_tree2

--- src/_10주차/TestBinaryTree.java
+++ src/_10주차/TestBinaryTree.java
@@ -6,8 +6,13 @@ public class TestBinaryTree {
 		// TODO Auto-generated method stub
 		BinaryTree tree = new BinaryTree(9);
 		BinaryTree tree2 = new BinaryTree(9, 30);
+		System.out.println("tree1");
 		tree.levelOrder();
 		System.out.print("pre Order :");
 		tree.preorder(0);
+		System.out.println("tree2");
+		tree2.levelOrder();
+		System.out.print("pre Order :");
+		tree2.preorder(0);
 	}
 }
