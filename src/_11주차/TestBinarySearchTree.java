9ef20047f60959f35ed0d8c2c8704f8765a15117
JeonSeongBae <tjdqo6042@naver.com>
2018-06-04 21:20:53 +0900

    revise testing code

--- src/_11주차/TestBinarySearchTree.java
+++ src/_11주차/TestBinarySearchTree.java
@@ -9,13 +9,19 @@ public class TestBinarySearchTree {
 		tree.recu_insert(1);
 		tree.recu_insert(3);
 		tree.recu_insert(7);
+		System.out.println("recu");
 		tree.inorder();
-		System.out.println("iter");
+
 		BinarySearchTree tree2 = new BinarySearchTree();
 		tree2.iter_insert(13);
 		tree2.iter_insert(11);
+		tree2.iter_insert(11);
+		tree2.iter_insert(11);
+		tree2.iter_insert(11);
+		tree2.iter_insert(11);
 		tree2.iter_insert(15);
 		tree2.iter_insert(12);
+		System.out.println("iter");
 		tree2.inorder();
 	}
 }
