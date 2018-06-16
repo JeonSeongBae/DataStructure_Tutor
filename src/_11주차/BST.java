4a9e75518718fd01180eb0c82c98d8999d69706a
JeonSeongBae <tjdqo6042@naver.com>
2018-06-16 17:51:32 +0900

    implement Interface

--- src/_11주차/BST.java
+++ src/_11주차/BST.java
@@ -2,7 +2,7 @@ package _11주차;
 
 public interface BST {
 	public int treesize();
-	boolean recu_insert(Object key);
-	boolean iter_insert(Object key);
+	boolean recu_insert(int key);
+	boolean iter_insert(int key);
 	public void inorder();
 }
